edf425cde468694aa9bdeae01d5178f2444fb181
Leonard907 <ou_litu@163.com>
2021-08-27 10:07:08 +0800

    Attempt JS

--- src/components/RaffleBlock.vue
+++ src/components/RaffleBlock.vue
@@ -1,107 +1,79 @@
 <template>
   <div class="home">
-    <!-- <h1>{{ msg }}</h1> -->
-    <!-- <h1>{{ prizesList }}</h1> -->
-    <!-- <ul>
-        <li
-          :ref="`item${val.order}`"
-          :data-index="val.order"
-          :class="val.order >= 1 ? 'rw' : ''"
-          v-for="val in resultList"
-          :key="val.prize_id"
+    <div class="prize-text">幸运抽奖</div>
+    <div class="money">当前金币值：{{ UserBanace }}</div>
+    <div class="container">
+      <div class="upper-border">
+        <div class="circle corner"></div>
+        <div class="circle hollow"></div>
+        <div class="circle center"></div>
+        <div class="circle hollow"></div>
+        <div class="circle corner"></div>
+      </div>
+      <div class="lower-border">
+        <div class="circle corner"></div>
+        <div class="circle hollow"></div>
+        <div class="circle center"></div>
+        <div class="circle hollow"></div>
+        <div class="circle corner"></div>
+      </div>
+      <div class="left-border">
+        <div class="circle corner"></div>
+        <div class="circle hollow"></div>
+        <div class="circle center"></div>
+        <div class="circle hollow"></div>
+        <div class="circle corner"></div>
+      </div>
+      <div class="right-border">
+        <div class="circle corner"></div>
+        <div class="circle hollow"></div>
+        <div class="circle center"></div>
+        <div class="circle hollow"></div>
+        <div class="circle corner"></div>
+      </div>
+      <div class="box" id="box0">
+        <img src="./img/ak47-asiimov.jpg" />{{ resultList[0].name }}
+      </div>
+      <div class="box" id="box1">
+        <img src="./img/awp-dragon.jpeg" />{{ resultList[1].name }}
+      </div>
+      <div class="box" id="box2">
+        <img src="./img/m4a4-howl.jpeg" />{{ resultList[2].name }}
+      </div>
+      <div class="box" id="box7">
+        <img src="./img/nothing.jpeg" />{{ resultList[7].name }}
+      </div>
+      <div class="start" id="boxcenter">
+        <el-button
+          class="startButton"
+          id="startButton"
+          @click="rotateWheel"
+          round
         >
-        <li :data-index="val.order" v-for="val in resultList" :key="val.order">
-          <div v-if="val.order !== -9">
-            <div class="name">{{ val.name }}</div>
-          </div>
-          <div v-else class="start" @click.stop.prevent="startGo"></div>
-        </li>
-      </ul> -->
-    <div class="prize-text">
-        幸运抽奖
-    </div>
-    <div class="money">
-        当前金币值：∞
-    </div>
-    <div class="wheel-container" style="width: 500px; height:500px">
-        <div class="upper-border">
-            <div class="circle corner"></div>
-            <div class="circle hollow"></div>
-            <div class="circle center"></div>
-            <div class="circle hollow"></div>
-            <div class="circle corner"></div>
-        </div>
-        <div class="lower-border">
-            <div class="circle corner"></div>
-            <div class="circle hollow"></div>
-            <div class="circle center"></div>
-            <div class="circle hollow"></div>
-            <div class="circle corner"></div>
-        </div>
-        <div class="left-border">
-            <div class="circle corner"></div>
-            <div class="circle hollow"></div>
-            <div class="circle center"></div>
-            <div class="circle hollow"></div>
-            <div class="circle corner"></div>
-        </div>
-        <div class="right-border">
-            <div class="circle corner"></div>
-            <div class="circle hollow"></div>
-            <div class="circle center"></div>
-            <div class="circle hollow"></div>
-            <div class="circle corner"></div>
-        </div>
-        <div class="prize-container">
-            <div class="prize">
-                <img src="./img/ak47-asiimov.jpg" class="image">
-                <p>AK47 | 二西莫夫</p>
-            </div>
-            <div class="prize">
-                <img src="./img/awp-dragon.jpeg" class="image">
-                <p>AWP | 巨龙传说</p>
-            </div>
-            <div class="prize">
-                <img src="./img/m4a4-howl.jpeg" class="image">
-                <p>M4A4 | 咆哮</p>
-            </div>
-            <div class="prize">
-                <img src="./img/nothing.jpeg" class="image">
-                <p>给你点个赞</p>
-            </div>
-            <div class="lottery">
-                <p>抽奖</p>
-            </div>
-            <div class="prize">
-                <img src="./img/nothing.jpeg" class="image">
-                <p>给你点个赞</p>
-            </div>
-            <div class="prize">
-                <img src="./img/m9-doppler.jpeg" class="image">
-                <p>M9刺刀 | 多普勒</p>
-            </div>
-            <div class="prize">
-                <img src="./img/pandora-gloves.jpeg" class="image">
-                <p>运动手套 | 潘多拉魔盒</p>
-            </div>
-            <div class="prize">
-                <img src="./img/fracture-case.jpeg" class="image">
-                <p>棱彩武器箱</p>
-            </div>
-        </div>
-        <div class="prize-window">
-            <p>恭喜你，你获得了</p>
-            <img id="prize-image" src="">
-            <p id="prize-content"></p>
-        </div>
+          <p>抽奖</p>
+          <p>10/次</p></el-button
+        >
+      </div>
+      <div class="box" id="box3">
+        <img src="./img/nothing.jpeg" />{{ resultList[3].name }}
+      </div>
+      <div class="box" id="box6">
+        <img src="./img/pandora-gloves.jpeg" />{{ resultList[6].name }}
+      </div>
+      <div class="box" id="box5">
+        <img src="./img/m9-doppler.jpeg" />{{ resultList[5].name }}
+      </div>
+      <div class="box" id="box4">
+        <img src="./img/fracture-case.jpeg" />{{ resultList[4].name }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 export default {
-  name: 'RaffleBlock',
+  name: "RaffleBlock",
   props: {
     msg: String,
     prizesList: String,
@@ -111,318 +83,300 @@ export default {
       // 先在本地预制一个奖品列表
       resultList: [
         {
-          name: '奖品1',
+          name: "AK47 | 二西莫夫",
           order: 1, // 顺序
-          probability: 12.5,
+          probability: 0.1,
         },
         {
-          name: '奖品1',
+          name: "AWP | 巨龙传说",
           order: 2,
-          probability: 12.5,
+          probability: 0.05,
         },
         {
-          name: '奖品1',
+          name: "M4A4 | 咆哮",
           order: 3,
-          probability: 12.5,
+          probability: 0.03,
         },
         {
-          name: '奖品1',
+          name: "给你点个赞",
           order: 4,
-          probability: 12.5,
+          probability: 40,
         },
         {
-          name: '奖品1',
+          name: "棱彩武器箱",
           order: 5,
-          probability: 12.5,
+          probability: 40,
         },
         {
-          name: '奖品1',
+          name: "M9刺刀 | 多普勒",
           order: 6,
-          probability: 12.5,
+          probability: 0.01,
         },
         {
-          name: '奖品1',
+          name: "运动手套 | 潘多拉魔盒",
           order: 7,
-          probability: 12.5,
+          probability: 0.01,
         },
         {
-          name: '奖品1',
+          name: "给你点个赞",
           order: 8,
-          probability: 12.5,
+          probability: 19.8,
         },
       ],
       // 后端返回的抽奖结果
       raffle: {
-        winNum: 0,
+        winNum: 2,
       },
       index: 0, //当前转动到哪个位置，起点位置
       stepDelay: 100, //初始转动速度 0.1s一步
       stepNum: 0,
-      IntervalID: '',
+      IntervalID: "",
       UserBanace: 200, // 存款，默认200
       isTurn: true, // 是否可以抽奖，后续加上余额判断
-      allPrizes: document.getElementById("prize-image")
-    }
+      pointerIndex: -1,
+    };
   },
   created() {
-    this.getRaffleList()
+    this.getRaffleList();
   },
   methods: {
-    // 要考虑一个网络断开的情况、点击开始抽奖的时候禁用box
-    start() {
-      console.log(this.allPrizes);
-      //this.stepNum = 40 + Math.ceil(Math.random() * 10) // 前端测试用JS模拟，抽奖结果由后端给出
-      this.index = 0
-      this.UserBanace -= 10
-      document.getElementById('startButton').disabled = true
-      this.getRaffleRes()
-      this.IntervalID = setInterval(this.rotate, this.stepDelay)
-    },
-    rotate() {
-      // 复原老格子style
-      let boxID = 'box' + this.index
-      let boxOld = document.getElementById(boxID)
-      boxOld.className = 'box'
-      if (this.index === 7) {
-        this.index = -1
+    rotateWheel() {
+      timeStamps = buildArray();
+      currentIndex = 0;
+      currentSteps = 1;
+      spin = setInterval(animate, 150);
+      finishedSpin = false;
+      function animate() {
+        if (finishedSpin) {
+          clearInterval(spin);
+        } else if (
+          currentIndex === timeStamps.length - 1 &&
+          currentSteps === timeStamps[currentIndex]
+        ) {
+          finishedSpin = true;
+          prizeText =
+            clockwisePrizes[pointerIndex].getElementsByTagName("p")[0]
+              .innerHTML;
+          prizeImage =
+            clockwisePrizes[pointerIndex].getElementsByTagName("img")[0].src;
+          prizeTextElement = document.getElementById("prize-content");
+          prizeImageElement = document.getElementById("prize-image");
+          prizeWindow = document.getElementsByClassName("prize-window")[0];
+          prizeImageElement.src = prizeImage;
+          prizeTextElement.innerHTML = prizeText;
+          prizeWindow.className = "prize-window show";
+        } else if (currentSteps === timeStamps[currentIndex]) {
+          movePrize();
+          currentIndex++;
+          currentSteps = 1;
+        } else {
+          currentSteps++;
+        }
       }
-      // 更新当前格子style
-      boxID = 'box' + ++this.index
-      var boxNow = document.getElementById(boxID)
-      boxNow.className = 'boxActived'
-      this.stepNum--
-      if (this.stepNum === 0) {
-        clearInterval(this.IntervalID)
-        this.index = 0
-        document.getElementById('startButton').disabled = false
-        // alert('恭喜你中奖了！') // UI待修改
+    },
+    getText(prize) {
+      return prize.innerHTML;
+    },
+    reset() {
+      for (i = 0; i < allPrizes.length; i++) {
+        allPrizes[i].className = "prize";
+      }
+      pointerIndex = -1;
+    },
+    movePrize() {
+      if (pointerIndex === -1) {
+        pointerIndex = 0;
       } else {
-        // 逐渐减速
-        clearInterval(this.IntervalID)
-        this.stepDelay = 1000 / this.stepNum
-        this.IntervalID = setInterval(this.rotate, this.stepDelay)
+        clockwisePrizes[pointerIndex].className = "prize";
+        pointerIndex++;
       }
+      if (pointerIndex === allPrizes.length) {
+        pointerIndex = 0;
+      }
+      clockwisePrizes[pointerIndex].className = "prize chosen";
     },
-    async getRaffleRes() {
-      const res = await axios({
-        url: 'http://localhost:3000/PrizeResult',
-        method: 'get',
-        responseType: 'json',
-      })
-        .then(function (response) {
-          return response.data.Num
-        })
-        .catch(function (error) {
-          console.log(error)
-        })
-      console.log('后台返回的抽奖结果是' + res)
-      this.raffle.winNum = res
-      this.stepNum = 40 + this.raffle.winNum
-    },
-    async getRaffleList() {
-      const res = await axios({
-        url: 'http://localhost:3000/RaffleList',
-        method: 'get',
-        responseType: 'json',
-      })
-        .then(function (response) {
-          return response.data.resultList
-        })
-        .catch(function (error) {
-          console.log('获取奖品列表失败：' + error)
-        })
-      this.resultList = res
-      // this.PrizeList = res
-      // console.log(this.PrizeList)
+    buildArray() {
+      timeStamps = new Array();
+      let i = 0;
+      let step = 8;
+      let finalPrize = Math.random() * 20;
+      for (i = 0; i < 3; i++) {
+        timeStamps[i] = step;
+        step /= 2;
+      }
+      for (; i < 30 + finalPrize; i++) {
+        timeStamps[i] = 1;
+      }
+      for (; i < 34 + finalPrize; i++) {
+        timeStamps[i] = step;
+        step *= 2;
+      }
+      return timeStamps;
     },
   },
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .home {
-  position: absolute;
+  display: flex;
+  flex-direction: column;
+  /* align-content: center; */
+  align-items: center;
+  /* justify-content: center; */
   background-color: rgb(27, 118, 209);
-  width: 100%;
-  height: 100%;
 }
-
-.prize-text, .money, .wheel-container {
-    margin-left: 500px;
-    color: rgb(230, 232, 217);
+.container {
+  display: flex;
+  width: 660px;
+  height: 660px;
+  background-color: orange;
+  flex-wrap: wrap;
+  align-content: center;
+  justify-content: center;
+  border: 50px solid #ffe6a6;
+  border-radius: 15%;
+  margin: 30px;
 }
-
-.prize-text::before {
-    content: "";
-    display: inline-block;
-    width: 10px;
-    height: 20px;
-    background-color: orange;
-    border-radius: 5px;
+.container > .start {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15%;
+  margin: 0%;
+  width: 180px;
+  height: 180px;
+  /* border: 10px solid rgb(112, 176, 250); */
+  background-color: rgb(255, 255, 255);
+  text-align: center;
+  line-height: 50px;
+  margin-right: 15px;
+  margin-top: 15px;
 }
-
+.box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border-radius: 30px;
+  /* margin: 0%; */
+  /* margin: 5.33px; */
+  width: 180px;
+  height: 180px;
+  /* border: 10px solid rgb(112, 176, 250); */
+  background-color: rgb(255, 255, 255);
+  text-align: center;
+  line-height: 50px;
+  margin-right: 15px;
+  margin-top: 15px;
+}
+.box:hover {
+  /* border: 10px solid red; */
+  box-shadow: inset 0 0 16px #ffa800;
+  z-index: 9999;
+}
+.start:hover {
+  /* border: 10px solid rgb(233, 186, 58); */
+  z-index: 9999;
+}
+.boxActived {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 15%;
+  margin: 0%;
+  width: 180px;
+  height: 180px;
+  text-align: center;
+  line-height: 50px;
+  margin-right: 15px;
+  margin-top: 15px;
+  background-color: #ffa800;
+  box-shadow: inset 0 0 16px #ffe6a6;
+}
+.startButton {
+  width: 180px;
+  height: 180px;
+  background-color: #ffe6a6;
+  box-shadow: inset 0 0 16px #ffa800;
+  align-items: center;
+  font-weight: bold;
+  font-size: 30px;
+  color: #a74b00;
+  cursor: pointer;
+}
 .prize-text {
-    margin-top: 100px;
-    font-size: 30px;
+  color: rgb(230, 232, 217);
+  border-radius: 5px;
+  margin-top: 100px;
+  font-size: 50px;
 }
-
 .money {
-    margin-top: 20px;
-    padding-left: 10px;
-    font-size: 20px;
+  margin-top: 20px;
+  padding-left: 10px;
+  font-size: 30px;
+  color: rgb(230, 232, 217);
 }
-
-.wheel-container {
-    margin-top: 50px;
-    width: 100%;
-    height: 100%;
-    border-radius: 30px;
-    background-color: #fadd95;
-    box-shadow: inset 0 0 16px orange;
+.upper-border,
+.lower-border,
+.left-border,
+.right-border {
+  position: absolute;
+  box-sizing: border-box;
+  display: flex;
+  justify-content: space-around;
 }
-
-.upper-border, .lower-border, .left-border, .right-border {
-    position: absolute;
-    box-sizing: border-box;
-    display: flex;
-    justify-content: space-around;
-}
-
-.upper-border, .lower-border {
-    margin-left: -25px;
-    flex-direction: row;
-    height: 12px;
-    width: 550px;
-}
-
-.left-border, .right-border {
-    margin-top: -30px;
-    flex-direction: column;
-    height: 550px;
-    width: 12px;
-}
-
 .upper-border {
-    margin-top: 5px;
+  transform: translateY(-50px);
+  margin-left: -25px;
+  flex-direction: row;
+  height: 12px;
+  width: 550px;
 }
-
 .lower-border {
-    margin-top: 445px;
+  transform: translateY(660px);
+  margin-left: -25px;
+  flex-direction: row;
+  height: 12px;
+  width: 550px;
 }
 
 .left-border {
-    margin-left: 15px;
+  transform: translateX(-365px);
+  margin-top: 50px;
+  flex-direction: column;
+  height: 550px;
+  width: 12px;
 }
-
 .right-border {
-    margin-left: 455px;
+  transform: translateX(345px);
+  margin-top: 50px;
+  flex-direction: column;
+  height: 550px;
+  width: 12px;
 }
-
 .circle {
-    border-radius: 50%;
-    box-sizing: border-box;
-    margin-top: 10px;
-    display: block;
-    width: 30px;
-    height: 30px;
+  border-radius: 50%;
+  box-sizing: border-box;
+  margin-top: 10px;
+  display: block;
+  width: 30px;
+  height: 30px;
 }
-
 .corner {
-    background-color: #e37815;
+  background-color: #e37815;
 }
-
 .hollow {
-    transform: translate(100%px);
-    background-color: transparent;
-    border: solid white 2px;
+  transform: translate(100%px);
+  background-color: transparent;
+  border: solid white 2px;
 }
-
 .center {
-    background-color: white;
+  background-color: white;
 }
-
-.prize-container, .prize-window, .prize-window.show {
-    position: absolute;
-    margin: 50px;
-    width: 400px;
-    height: 400px;
-    border-radius: 30px;
-    background-color: orange;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    flex-direction: column;
-    align-content: space-between;
-}
-
-.prize, .lottery {
-    margin: 5.33px;
-    border-radius: 30px;
-    background-color: white;
-    width: 30%;
-    height: 30%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-
-.lottery {
-    background-color: #ffe6a6;
-    box-shadow: inset 0 0 16px #ffa800;
-}
-
-.image {
-    width: 80%;
-    height: 60%;
-    align-items: center;
-    justify-content: center;
-    display: flex;
-}
-
-.prize p, .prize-window p {
-    color: #d25f00;
-    font-size: 10px;
-    align-items: center;
-}
-
-.lottery p {
-    align-items: center;
-    font-weight: bold;
-    font-size: large;
-    color: #a74b00;
-    cursor: pointer;
-}
-
-.prize.chosen {
-    background-color: #ffa800;
-    box-shadow: inset 0 0 16px #ffe6a6;
-}
-
-.prize-window {
-    background-color: white;
-    display: none;
-}
-
-.prize-window.show {
-    background-color: white;
-    display: block;
-    text-align: center;
-}
-
-.prize-window p {
-    font-size: 30px;
-}
-
-#prize-content  {
-    font-weight: bold;
-}
-
-#prize-image {
-    margin-left: auto;
-    margin-right: auto;
-    width: 120px;
-    height:120px;
+img {
+  width: 80%;
+  height: 60%;
 }
 </style>
